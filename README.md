d84804f65ed234f281f3de6dc045445ff5db3ff3
derhaideer2 <muhammedemir.akguel@gmail.com>
2025-08-29 19:06:18 +0200

    Adding fundamentals

---
+++ README.md
@@ -0,0 +1,2 @@
+**A Little Projekt by me, Muhammed Emir Akgül, where I try to make a game with the pygame framework, trying to learn the fundementals of professional programming like clean Documentation, testing, CI/CD pipelining, reusable and sustainable code...**
+
--- src/main.py
+++ src/main.py
@@ -1,1 +1,38 @@
-print(a)
+import pygame
+from sys import exit
+import random
+
+import pygame.docs
+
+#inital declarations
+pygame.init()
+width = 1000
+height = 600
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption("Multigame World")
+clock = pygame.time.Clock()
+gameStatus = True
+
+
+
+
+
+
+
+#game loop
+while gameStatus:
+    
+    for event in pygame.event.get(): 
+        #Quit-Event Handling
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+    
+    
+    
+    
+    
+    
+    
+    
+    pygame.display.update()
